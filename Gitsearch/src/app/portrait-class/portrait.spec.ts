bcb327e6c12e70278c61e0190677714e05341f8b
AliKheir <akheirali@gmail.com>
2019-09-29 17:48:53 +0300

    added potrait function to portrait.spec

--- Gitsearch/src/app/portrait-class/portrait.spec.ts
+++ Gitsearch/src/app/portrait-class/portrait.spec.ts
@@ -1,25 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Portrait } from './portrait';
 
-import { PortraitClassComponent } from './portrait';
 
-describe('PortraitClassComponent', () => {
-  let component: PortraitClassComponent;
-  let fixture: ComponentFixture<PortraitClassComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PortraitClassComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PortraitClassComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+describe('Portrait', () => {
+  it('should create an instance', () => {
+    // expect(new Portrait()).toBeTruthy();
   });
 });
