ceb6719ab173ec838864d27280f7cd91813993d1
AliKheir <akheirali@gmail.com>
2019-09-29 18:28:53 +0300

    added getGithub user directive and ngOnInit to user comp

---
+++ Gitsearch/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortraitClassComponent } from './portrait-class/portrait';
+import { TheUserComponent } from './the-user/the-user.component';
+import { ViewComponent } from './view/view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortraitClassComponent,
+    TheUserComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
